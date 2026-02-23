61269ee28c21060982966f0301ccaba4d7b54595
cellotak <tak.2cellos.ashi.777@gmail.com>
2026-02-23 17:04:28 +0900

    cal.jsを作成、オプションを受け取り年月だけ1行目の曜日を出力する状態まで実装

---
+++ 02.calendar/cal.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+
+const argv = minimist(process.argv.slice(2));
+const today = new Date();
+
+const year = argv.y || today.getFullYear();
+const month = argv.m || today.getMonth() + 1;
+
+console.log(`      ${month}月 ${year}`);
+console.log("日 月 火 水 木 金 土");
