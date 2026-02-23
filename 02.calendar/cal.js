9b2654e8a4b4be8c2d2fcfa662b69bc226a03764
cellotak <tak.2cellos.ashi.777@gmail.com>
2026-02-23 17:06:26 +0900

    対象付きの1日目の曜日、および末日を算出する処理を追加

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -10,3 +10,12 @@ const month = argv.m || today.getMonth() + 1;
 
 console.log(`      ${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
+
+const firstDate = new Date(year, month - 1, 1);
+const firstDayOfWeek = firstDate.getDay();
+
+const lastDate = new Date(year, month, 0);
+const lastDay = lastDate.getDate();
+
+console.log(`対象月の1日の曜日の数値: ${firstDayOfWeek}`);
+console.log(`対象月の末日: ${lastDay}`);
