a7aaae7d44fd01260cac2aac77c5e4842b567f1f
cellotak <tak.2cellos.ashi.777@gmail.com>
2026-06-06 23:09:48 +0900

    Promiseでの実装

---
+++ 03.asynchronous/02_promise_error.js
@@ -0,0 +1,19 @@
+import { db, run, all } from "./db.js";
+
+run(
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() =>
+    run("INSERT INTO books (title) VALUES (?)", [null]).catch((err) =>
+      console.error(err.message),
+    ),
+  )
+  .then(() => {
+    all("SELECT * FROM nonexistent_table").catch((err) =>
+      console.error(err.message),
+    );
+  })
+  .then(() => run("DROP TABLE books"))
+  .then(() => {
+    db.close();
+  });
